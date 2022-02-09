ec0c0d3028bc0b60f23512be225259ee916b6353
MrKillerVortex <shawn-2002@hotmail.com>
2022-02-09 12:51:39 -0500

    Setup GitHub
    Setup avec tout dedans pour aider

---
+++ site/introcss/page2.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Notions d'introduction sur HTML/CSS/JavaScript et PHP</title>
+        <!-- <script src="intro.css" type="text/css"></script> -->
+        <link href="intro.css" rel="stylesheet">
+        <style>
+            p {
+                background-color: yellow;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 id="titre1">Introduction des notions HTML et CSS</h1>
+        <p style="background-color: chocolate;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate pretium velit in mollis. Suspendisse eu condimentum massa. Praesent tempus venenatis metus, vel euismod felis dignissim in. Nam euismod viverra odio, et tincidunt ipsum ultricies sed. Phasellus et ligula lectus. Sed sed dictum massa. In laoreet posuere odio sit amet ullamcorper.
+        </p>
+        <p>Ut id molestie neque. Mauris quis fermentum ligula. Sed gravida enim a lorem maximus, non viverra nibh imperdiet. Proin fermentum sapien id lacus suscipit, sit amet ullamcorper justo interdum. Quisque nec vulputate metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin nec eros massa. Vivamus tincidunt neque at mollis fringilla. Proin tempor mauris quis hendrerit luctus. In ultrices velit ac mauris pharetra finibus. Fusce eros diam, mattis at vestibulum sed, congue a augue.</p>
+
+    </body>
+</html>
